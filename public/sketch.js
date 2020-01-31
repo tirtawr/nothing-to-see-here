7e430b25daef9250798d6dc45e444b3031a64c2f
Tirta Rachman <tirtawening@gmail.com>
2020-01-31 17:10:54 -0500

    Add p5 sketch

---
+++ public/sketch.js
@@ -0,0 +1,8 @@
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+function draw() {
+    textSize(32);
+    text('Nothing to see here', random(windowWidth), random(windowHeight));
+}
